5eadb9db4befadee6d26e774449143a6d21659d2
Andreas Kaeberlein <andreas.kaeberlein.ext@siemens.com>
2018-05-25 08:50:42 +0200

    pciinfo.h -> added verbose set function to header file -> corrected style of doxygen entries

--- pciinfo.h
+++ pciinfo.h
@@ -24,14 +24,18 @@
 #define __PCI_PCIINFO
 
 
-/** @brief	looks in linux system path for PCI devices with given
- * 			VendorID and DeviceID
- * 
- * 	@param[in]	vendorID		PCI device vendor
- *	@param[in]	deviceID		PCI device identification
- * 	@param[out]	devicePath		linux system path to PCI device
- * 	@param[in]	devicePathMax	maximal length of 'devicePath'
- * 	@return		1: no match; 0: unique, >0: multiple
+/** @brief	PCI device root dir in file system
+ * 			
+ * 	looks in linux system path for PCI devices with given VendorID and DeviceID
+ *
+ * 	@param[in]	char				PCI device vendor
+ * 	@param[in]	char				PCI device identification
+ * 	@param[out]	char				linux system path to PCI device
+ * 	@param[in]	uint32_t			maximal length of 'devicePath'
+ * 	@return		int					search result
+ *	@retval		0					Unique Vendor Device ID match
+ *	@retval		-1					no match
+ *  @retval		1					multiple matches
  * 	@since 		2016-10-13
  * 	@author		Andreas Kaeberlein
  */
@@ -39,11 +43,15 @@ int pciinfoFind(char vendorID[], char deviceID[], char devicePath[], uint32_t de
 
 
 
-/** @brief	extracts sizes of PCI Bars for given PCI device
+/** @brief	get BAR sizes
  * 
- * 	@param[in]	sysPathPciDev	system path to pci device
- * 	@param[out]	byteSize		six elements, sizes of bar in byte
- * 	@return		state
+ * 	extracts sizes of PCI Bars for given PCI device
+ *
+ * 	@param[in]	char				system path to pci device
+ * 	@param[out]	uint32_t			array of uint32_t with six elements, sizes of BAR in byte
+ * 	@return		int					state
+ *	@retval		0					OK
+ *	@retval		-1					FAIL
  * 	@since 		2016-10-14
  * 	@author		Andreas Kaeberlein
  */
@@ -51,15 +59,18 @@ int pciinfoBarSize(char sysPathPciDev[], uint32_t byteSize[]);
 
 
 
-/** @brief	looks in linux system path for PCI devices with given
- * 			VendorID and DeviceID
+/** @brief	get filesystem path to BAR of PCI device
+ * 			
+ * 	looks in linux system path for PCI devices with given VendorID and DeviceID
  * 
- * 	@param[in]	vendorID		PCI device vendor
- *	@param[in]	deviceID		PCI device identification
- *	@param[in]	bar				PCI bar
- * 	@param[out]	devicePath		linux system path to PCI device
- * 	@param[in]	devicePathMax	maximal length of 'devicePath'
- * 	@return		1: no match; 0: unique, >0: multiple
+ * 	@param[in]	char				PCI device vendor
+ *	@param[in]	char				PCI device identification
+ * 	@param[out]	char				linux system path to PCI device
+ * 	@param[in]	uint32_t			maximal length of 'devicePath'
+ * 	@return		int					search result
+ *	@retval		0					Unique Vendor Device ID match
+ *	@retval		-1					no match
+ *  @retval		1					multiple matches
  * 	@since 		2017-02-07
  * 	@author		Andreas Kaeberlein
  */
@@ -69,14 +80,30 @@ int pciinfoBarPath(char vendorID[], char deviceID[], uint8_t bar, char devicePat
 
 /** @brief	extracts physical address of given PCI Bars
  * 
- * 	@param[in]	sysPathPciDev	system path to pci device
- * 	@param[in]	barNo			pci device bar number
- * 	@param[out]	barPhyAddr		physical address
- * 	@return		state
+ * 	@param[in]	char				system path to pci device
+ * 	@param[in]	uint8_t				pci device bar number
+ * 	@param[out]	uint32_t			physical address
+ * 	@return		int					state
+ *	@retval		0					OK
+ *	@retval		-1					FAIL
  * 	@since 		2017-05-08
  * 	@author		Kartik Desai
  */
 int pciinfoBarPhyAddr(char sysPathPciDev[], uint8_t barNo, uint32_t * barPhyAddr);
+
+
+
+/** @brief	set verbose level
+ * 
+ *  enabling disabling of pciinfo addional debug information
+ * 
+ * 	@param[in]	uint8_t				PCI device vendor
+ * 	@return		int					state
+ *  @retval		0					OK
+ * 	@since 		2018-05-25
+ * 	@author		Andreas Kaeberlein
+ */
+void pciinfoSetVerboseLevel(uint8_t level);
 
 
 //--------------------------------------------------------------
