3c26b6abfd7ba9b6f2c2430beb8c066a4e93c7aa
Desai Kartik <kartik.desai@siemens.com>
2018-08-09 09:02:46 +0200

    pciinfo
      -> code foramting changed according to the Google codding guidelines
      -> in the function pciinfoBarPhyAddr, returned value of the file
      handle checked for the error

--- pciinfo.c
+++ pciinfo.c
@@ -1,47 +1,43 @@
-/***********************************************************************
- Copyright		: (c) All Rights Reserved
- Company		: Siemens AG
- Address		: Clemens-Winkler-Strasse 3, 09116 Chemnitz
- Telephone		: +49 371 4750
+/******************************************************************************
+ @Copyright          : (c) All Rights Reserved
+   Company           : Siemens AG
+   Address           : Clemens-Winkler-Strasse 3, 09116 Chemnitz
+   Telephone         : +49 371 4750
 
- @author		: Andreas Kaeberlein
-
- eMail			: andreas.kaeberlein.ext@siemens.com
-
- @file			: pcimemio.c
-
- @brief			: function to get infos about pci devices
-
- @date			: 2017-01-30
-***********************************************************************/
-
+ @Author             : Kaeberlein, Andreas <andreas.kaeberlein.ext@siemens.com>
+ @Date               : 2017-01-30 08:51:15
+ @File               : pciinfo.c
+ @Version            :
+ @Brief              : function to get infos about pci devices
+ @Last Modified by   : z003su8e
+ @Last Modified time : 2018-08-09 10:49:28
+*******************************************************************************/
 
 
 /** Includes **/
 /* Standard libs */
-#include <stdio.h>			// f.e. printf
-#include <stdlib.h>			// defines four variables, several macros,
-							// and various functions for performing
-							// general functions
-#include <stdint.h>			// defines fixed data types, like int8_t...
-#include <unistd.h>			// system call wrapper functions such as fork, pipe and I/O primitives (read, write, close, etc.).
-#include <string.h>			// string handling functions
-#include <stdarg.h>			// variable parameter lists
+#include <stdio.h>    /* e.g. printf */
+#include <stdlib.h>   /* defines four variables, several macros, and various
+                       * functions for performing general operations */
+#include <stdarg.h>   /* variable parameter lists */
+#include <stdint.h>   /* defines fixed data types, like int8_t... */
+#include <string.h>   /* string handling functions */
+#include <unistd.h>   /* system call wrapper functions such as fork, pipe
+                       * and I/O primitives (read, write, close, etc.) */
 
 /* User libs */
-#include "pciinfo.h"		// function prototypes
+#include "pciinfo.h"  /* function prototypes */
 
 
 
-
- /** globals **/
+/** globals **/
 volatile uint8_t uint8pciinfoVerboseLevel = 0;
 
 
 
-/** 
+/**
  *  pciinfoVerbosePrint
- * 	-------------------
+ *  -------------------
  */
 void pciinfoVerbosePrint(char *template, ...)
 {
@@ -49,19 +45,18 @@ void pciinfoVerbosePrint(char *template, ...)
 	va_list ap;
 
 	/* switch print off */
-	if (uint8pciinfoVerboseLevel != 0)
-	{
+	if (0 != uint8pciinfoVerboseLevel) {
 		va_start(ap, template);
 		vprintf(template, ap);
 		va_end(ap);
-	};
+	}
 }
 
 
 
-/** 
+/**
  *  setVerboseLevel
- * 	---------------
+ *  ---------------
  */
 void pciinfoSetVerboseLevel(uint8_t level)
 {
@@ -70,22 +65,21 @@ void pciinfoSetVerboseLevel(uint8_t level)
 
 
 
-/** 
+/**
  *  pciinfoFind
- * 	-----------
+ *  -----------
  */
-int pciinfoFind(char vendorID[], char deviceID[], char devicePath[], uint32_t devicePathMax)
+int pciinfoFind(char vendorID[], char deviceID[], char devicePath[],
+                uint32_t devicePathMax)
 {
 	/** used variables **/
-	char 		cmd[256]; 					// command buffer
-	char		devPath[256];				// path buffer
-	char 		line[1024], line2[1024];	// read buffer
-	uint32_t	uint32DevPathIdx;			// match with buff
-	uint8_t		uint8FoundDevice;			// device found
-	FILE 		*foundVendor;				// system call answer
-	FILE		*foundDevice;
-
-
+	char     cmd[256];                 /* command buffer */
+	char     devPath[256];             /* path buffer */
+	char     line1[1024], line2[1024]; /* read buffer */
+	uint32_t uint32DevPathIdx;         /* match with buff */
+	uint8_t  uint8FoundDevice;         /* device found */
+	FILE     *foundVendor;             /* system call answer */
+	FILE     *foundDevice;
 
 	/* function call message */
 	pciinfoVerbosePrint("__FUNCTION__ = %s\n", __FUNCTION__);
@@ -96,34 +90,35 @@ int pciinfoFind(char vendorID[], char deviceID[], char devicePath[], uint32_t de
 	foundVendor = popen(cmd, "r");
 
 	/* process found device list for device ID */
-	uint8FoundDevice  = 0;
-	while(fscanf(foundVendor, "%s", line) != EOF) {	
-		/* 	extract device path for given vendor id
-		 * 	before: /sys/bus/pci/devices/0000:03:0d.0/vendor:1:0x110a
-		 * 	after:	/sys/bus/pci/devices/0000:03:0d.0
+	uint8FoundDevice = 0;
+	while (EOF != fscanf(foundVendor, "%s", line1)) {
+		/*  extract device path for given vendor id
+		 *  before: /sys/bus/pci/devices/0000:03:0d.0/vendor:1:0x110a
+		 *  after:  /sys/bus/pci/devices/0000:03:0d.0
 		 */
-		if (strstr(line, "/vendor")-line < (int)(sizeof(devPath)/sizeof(devPath[0]))) {
-			uint32DevPathIdx = strstr(line, "/vendor")-line;
+		if ( (strstr(line1, "/vendor") - line1) <
+		    ((int)(sizeof(devPath) / sizeof(devPath[0]))) ) {
+			uint32DevPathIdx = strstr(line1, "/vendor") - line1;
 		} else {
 			uint32DevPathIdx = 0;
 			pciinfoVerbosePrint("Not enough memory to store PCI device path...\n");
 		}
-		strncpy(devPath, line, uint32DevPathIdx);
-		devPath[uint32DevPathIdx] = '\0';		// termination character
+		strncpy(devPath, line1, uint32DevPathIdx);
+		devPath[uint32DevPathIdx] = '\0';    /* termination character */
 
-		/*	build command for device id look up
-		 * 	cat /sys/bus/pci/devices/0000:03:0d.0/device
-		*/
+		/*  build command for device id look up
+		 *  cat /sys/bus/pci/devices/0000:03:0d.0/device
+		 */
 		strcpy(cmd, "cat ");
 		strcat(cmd, devPath);
 		strcat(cmd, "/device");
 		foundDevice = popen(cmd, "r");
 
 		/* process system call response */
-		if ( fscanf(foundDevice, "%s", line2) ) {	// read only first line
+		if ( fscanf(foundDevice, "%s", line2) ) {    /* read only first line */
 		}
-		if (strcmp(line2, deviceID) == 0) {
-			++uint8FoundDevice;				// increment match counter
+		if (0 == strcmp(line2, deviceID)) {
+			++uint8FoundDevice;    /* increment match counter */
 			strncpy(devicePath, devPath, devicePathMax);
 			pciinfoVerbosePrint("vendor=%s/device=%s in '%s'\n", vendorID, line2, devPath);
 		}
@@ -136,77 +131,76 @@ int pciinfoFind(char vendorID[], char deviceID[], char devicePath[], uint32_t de
 	pclose(foundVendor);
 
 	/* finish function */
-	return uint8FoundDevice-1;
+	return (uint8FoundDevice - 1);
 }
 
 
 
-/** 
+/**
  *  pciinfoBarSize
- * 	--------------
+ *  --------------
  */
 int pciinfoBarSize(char sysPathPciDev[], uint32_t byteSize[])
 {
 
 	/** used variables **/
-	char 	cmd[256], rawCmd[256];		// string for buffering command
-	char 	resultLine[256];			// command result one file line
-	uint8_t	i, j;						// iterator
-	FILE 	*cmdResult;					// stored command result
-
-
+	char    cmd[256], rawCmd[256]; /* string for buffering command */
+	char    resultLine[256];       /* command result one file line */
+	uint8_t i, j;                  /* iterator */
+	FILE    *cmdResult;            /* stored command result */
 
 	/* function call message */
 	pciinfoVerbosePrint("__FUNCTION__ = %s\n", __FUNCTION__);
 	pciinfoVerbosePrint("device: '%s'\n", sysPathPciDev);
-	
+
 	/* init bar data */
-	for (i=0; i<6; i++) {
-		byteSize[i]	= 0;
+	for (i = 0; i < 6; i++) {
+		byteSize[i] = 0;
 	}
 
-	/*	build system call request for bar size 
-	 * 	ls -la /sys/bus/pci/devices/0000:03:0d.0/resource*
+	/*  build system call request for bar size
+	 *  ls -la /sys/bus/pci/devices/0000:03:0d.0/resource*
 	 */
-	if (7+strlen(sysPathPciDev)+9+1+5 < sizeof(rawCmd)/sizeof(rawCmd[0])) {
-		strcpy(rawCmd, "ls -la ");			// 7 chars
+	if ( (7 + strlen(sysPathPciDev) + 9 + 1 + 5) <
+	    (sizeof(rawCmd) / sizeof(rawCmd[0])) ) {
+		strcpy(rawCmd, "ls -la ");      /* 7 chars */
 		strcat(rawCmd, sysPathPciDev);
-		strcat(rawCmd, "/resource");		// 9 chars
+		strcat(rawCmd, "/resource");    /* 9 chars */
 	} else {
 		pciinfoVerbosePrint("To few memory allocated ...abort\n");
-		rawCmd[0] = '\0';	
+		rawCmd[0] = '\0';
 		return -1;
-	};
+	}
 
 	/* iterate over six possible pci bars */
-	for (i=0; i<6; i++) {
+	for (i = 0; i < 6; i++) {
 		/* build final command and execute*/
-		sprintf(cmd, "%s%d 2>&1", rawCmd, i);	// 2>&1 redirect stderr
+		sprintf(cmd, "%s%d 2>&1", rawCmd, i);    /* 2>&1 redirect stderr */
 		cmdResult = popen(cmd, "r");
 
 		/* process acquired data */
 		j = 0;
-		while(fscanf(cmdResult, "%s", resultLine) != EOF) {
-			/* 	get fifth element in table, is file size 
-			 * 	-rw------- 1 root root 32768 Feb 11 11:40 /sys/bus/pci/devices/0000:03:0d.0/resource0
+		while (EOF != fscanf(cmdResult, "%s", resultLine)) {
+			/*  get fifth element in table, is file size
+			 *  -rw------- 1 root root 32768 Feb 11 11:40 /sys/bus/pci/devices/0000:03:0d.0/resource0
 			 */
-			if (j == 4) {
+			if (4 == j) {
 				byteSize[i] = strtoll(resultLine, NULL, 10);
 			}
-			++j;		// increment counter
+			++j;    /* increment counter */
 		}
-		
+
 		/* close file handle */
 		pclose(cmdResult);
 	}
-	
+
 	/* print bar sizes */
 	pciinfoVerbosePrint("\tSizes:\n");
 	pciinfoVerbosePrint("\t------\n");
-	for (i=0; i<6; i++) {
+	for (i = 0; i < 6; i++) {
 		pciinfoVerbosePrint("\tBAR%d:%I32u Byte\n", i, byteSize[i]);
 	}
-	
+
 	/* finish function */
 	return 0;
 }
@@ -215,22 +209,25 @@ int pciinfoBarSize(char sysPathPciDev[], uint32_t byteSize[])
 
 /**
  *  pciinfoBarPath
- * 	--------------
+ *  --------------
  */
-int pciinfoBarPath(char vendorID[], char deviceID[], uint8_t bar, char devicePath[], uint32_t devicePathMax)
+int pciinfoBarPath(char vendorID[], char deviceID[], uint8_t bar,
+                   char devicePath[], uint32_t devicePathMax)
 {
 
 	/* function call message */
 	pciinfoVerbosePrint("__FUNCTION__ = %s\n", __FUNCTION__);
 
-   	/* find pci device */
-	if (pciinfoFind(vendorID, deviceID, devicePath, devicePathMax) != 0) {
-		pciinfoVerbosePrint("Unable to find PCI Device with VendorID=%s and DeviceID=%s\n", vendorID, deviceID);
+	/* find pci device */
+	if (0 != pciinfoFind(vendorID, deviceID, devicePath, devicePathMax)) {
+		pciinfoVerbosePrint(
+		    "Unable to find PCI Device with VendorID=%s and DeviceID=%s\n",
+		    vendorID, deviceID);
 		return -1;
 	}
 
 	/* build final path */
-	if (strlen(devicePath)+10 < devicePathMax) {
+	if ((strlen(devicePath) + 10) < devicePathMax) {
 		sprintf(devicePath, "%s%s%d", devicePath, "/resource", bar);
 	} else {
 		pciinfoVerbosePrint("To few memory allocated ...abort\n");
@@ -243,17 +240,18 @@ int pciinfoBarPath(char vendorID[], char deviceID[], uint8_t bar, char devicePat
 
 
 
-/** 
+/**
  *  pciinfoBarPhyAddr
- * 	-----------------
+ *  -----------------
  */
-int pciinfoBarPhyAddr(char sysPathPciDev[], uint8_t barNo, uint32_t * barPhyAddr)
+int pciinfoBarPhyAddr(char sysPathPciDev[], uint8_t barNo,
+                      uint32_t *barPhyAddr)
 {
 	/** used variables **/
-	char 	line[1023];					// read buffer
-	char	sysPathTemp[1024] = {'\0'};	// temporary buffer variable
-	FILE 	* fptr;
+	char    sysPathTemp[1024] = {'\0'}; /* temporary buffer variable */
+	char    line[1023];                 /* read buffer */
 	uint8_t index;
+	FILE    *fptr;
 
 	/* function call message */
 	pciinfoVerbosePrint("__FUNCTION__ = %s\n", __FUNCTION__);
@@ -263,28 +261,31 @@ int pciinfoBarPhyAddr(char sysPathPciDev[], uint8_t barNo, uint32_t * barPhyAddr
 	*  before: /sys/bus/pci/devices/0000:04:0d.0/
 	*  after:  /sys/bus/pci/devices/0000:04:0d.0/resource
 	*/
-	strncpy(sysPathTemp, sysPathPciDev, sizeof(sysPathTemp)/sizeof(sysPathTemp[0]));
+	strncpy(sysPathTemp, sysPathPciDev,
+	        (sizeof(sysPathTemp) / sizeof(sysPathTemp[0])));
 	strcat(sysPathTemp, "/resource");
 
 	/* open file handle */
 	fptr = fopen(sysPathTemp, "r");
+	if (NULL == fptr) {
+		pciinfoVerbosePrint("ERROR: failed to open '%s'.\n", sysPathTemp);
+		return -1;
+	}
 
 	/* iterate over all possible pci bars */
 	/* line number is the bar number */
-	for (index = 0; index < barNo; index++)
-	{
-		if (fgets(line, sizeof(line), fptr) == NULL) 
-		{
+	for (index = 0; index < barNo; index++) {
+		if (NULL == fgets(line, sizeof(line), fptr)) {
 			pciinfoVerbosePrint("ERROR: bar %s not resent\n", barNo);
 			fclose(fptr);
 			return -1;
 		}
 	}
 	/* first column is the starting address of the bar */
-	if ( fscanf(fptr, "%x", barPhyAddr) ){
+	if ( fscanf(fptr, "%x", barPhyAddr) ) {
 	}
 
-    /* close opened pipe */
+	/* close opened pipe */
 	fclose(fptr);
 
 	/* finish function */
--- pciinfo.h
+++ pciinfo.h
@@ -1,19 +1,17 @@
-/***********************************************************************
- Copyright		: (c) All Rights Reserved
- Company		: Siemens AG
- Address		: Clemens-Winkler-Strasse 3, 09116 Chemnitz
- Telephone		: +49 371 4750
+/******************************************************************************
+ @Copyright          : (c) All Rights Reserved
+   Company           : Siemens AG
+   Address           : Clemens-Winkler-Strasse 3, 09116 Chemnitz
+   Telephone         : +49 371 4750
 
- @author		: Andreas Kaeberlein
-
- eMail			: andreas.kaeberlein.ext@siemens.com
-
- @file			: pciinfo.h
-
- @brief			: function to get infos about pci devices
-
- @date			: 2017-01-30
-***********************************************************************/
+ @Author             : Kaeberlein, Andreas <andreas.kaeberlein.ext@siemens.com>
+ @Date               : 2017-01-30 08:51:15
+ @File               : pciinfo.h
+ @Version            :
+ @Brief              : function to get infos about pci devices
+ @Last Modified by   : z003su8e
+ @Last Modified time : 2018-08-09 09:45:49
+*******************************************************************************/
 
 
 
@@ -24,89 +22,92 @@
 #define __PCI_PCIINFO
 
 
-/** @brief	PCI device root dir in file system
- * 			
- * 	looks in linux system path for PCI devices with given VendorID and DeviceID
+
+/** @brief  PCI device root dir in file system
  *
- * 	@param[in]	char				PCI device vendor
- * 	@param[in]	char				PCI device identification
- * 	@param[out]	char				linux system path to PCI device
- * 	@param[in]	uint32_t			maximal length of 'devicePath'
- * 	@return		int					search result
- *	@retval		0					Unique Vendor Device ID match
- *	@retval		-1					no match
- *  @retval		1					multiple matches
- * 	@since 		2016-10-13
- * 	@author		Andreas Kaeberlein
+ *  looks in Linux system path for PCI devices with given VendorID and DeviceID
+ *
+ *  @param[in]  char        PCI device vendor
+ *  @param[in]  char        PCI device identification
+ *  @param[out] char        Linux system path to PCI device
+ *  @param[in]  uint32_t    maximal length of 'devicePath'
+ *  @return     int         search result
+ *  @retval     0           Unique Vendor Device ID match
+ *  @retval     -1          no match
+ *  @retval     1           multiple matches
+ *  @since      2016-10-13
+ *  @author     Andreas Kaeberlein
  */
-int pciinfoFind(char vendorID[], char deviceID[], char devicePath[], uint32_t devicePathMax);
+int pciinfoFind(char vendorID[], char deviceID[], char devicePath[],
+                uint32_t devicePathMax);
 
 
 
-/** @brief	get BAR sizes
- * 
- * 	extracts sizes of PCI Bars for given PCI device
+/** @brief  get BAR sizes
  *
- * 	@param[in]	char				system path to pci device
- * 	@param[out]	uint32_t			array of uint32_t with six elements, sizes of BAR in byte
- * 	@return		int					state
- *	@retval		0					OK
- *	@retval		-1					FAIL
- * 	@since 		2016-10-14
- * 	@author		Andreas Kaeberlein
+ *  extracts sizes of PCI Bars for given PCI device
+ *
+ *  @param[in]  char       system path to PCI device
+ *  @param[out] uint32_t   array of uint32_t with six elements, sizes of BAR in byte
+ *  @return     int        state
+ *  @retval     0          OK
+ *  @retval     -1         FAIL
+ *  @since      2016-10-14
+ *  @author     Andreas Kaeberlein
  */
 int pciinfoBarSize(char sysPathPciDev[], uint32_t byteSize[]);
 
 
 
-/** @brief	get filesystem path to BAR of PCI device
- * 			
- * 	looks in linux system path for PCI devices with given VendorID and DeviceID
- * 
- * 	@param[in]	char				PCI device vendor
- *	@param[in]	char				PCI device identification
- * 	@param[out]	char				linux system path to PCI device
- * 	@param[in]	uint32_t			maximal length of 'devicePath'
- * 	@return		int					search result
- *	@retval		0					Unique Vendor Device ID match
- *	@retval		-1					no match
- *  @retval		1					multiple matches
- * 	@since 		2017-02-07
- * 	@author		Andreas Kaeberlein
- */
-int pciinfoBarPath(char vendorID[], char deviceID[], uint8_t bar, char devicePath[], uint32_t devicePathMax);
-
-
-
-/** @brief	get physical bar address
- * 
- *	extracts physical address of given PCI Bars
+/** @brief  get file-system path to BAR of PCI device
  *
- * 	@param[in]	char				system path to pci device
- * 	@param[in]	uint8_t				pci device bar number
- * 	@param[out]	uint32_t			physical address
- * 	@return		int					state
- *	@retval		0					OK
- *	@retval		-1					FAIL
- * 	@since 		2017-05-08
- * 	@author		Kartik Desai
+ *  looks in Linux system path for PCI devices with given VendorID and DeviceID
+ *
+ *  @param[in]  char        PCI device vendor
+ *  @param[in]  char        PCI device identification
+ *  @param[out] char        Linux system path to PCI device
+ *  @param[in]  uint32_t    maximal length of 'devicePath'
+ *  @return     int         search result
+ *  @retval     0           Unique Vendor Device ID match
+ *  @retval     -1          no match
+ *  @retval     1           multiple matches
+ *  @since      2017-02-07
+ *  @author     Andreas Kaeberlein
  */
-int pciinfoBarPhyAddr(char sysPathPciDev[], uint8_t barNo, uint32_t * barPhyAddr);
+int pciinfoBarPath(char vendorID[], char deviceID[], uint8_t bar,
+                   char devicePath[], uint32_t devicePathMax);
 
 
 
-/** @brief	set verbose level
- * 
- *  enabling disabling of pciinfo addional debug information
- * 
- * 	@param[in]	uint8_t				PCI device vendor
- * 	@return		int					state
- *  @retval		0					OK
- * 	@since 		2018-05-25
- * 	@author		Andreas Kaeberlein
+/** @brief  get physical bar address
+ *
+ *  extracts physical address of given PCI Bars
+ *
+ *  @param[in]  char        system path to PCI device
+ *  @param[in]  uint8_t     PCI device bar number
+ *  @param[out] uint32_t    physical address
+ *  @return     int         state
+ *  @retval     0           OK
+ *  @retval     -1          FAIL
+ *  @since      2017-05-08
+ *  @author     Kartik Desai
+ */
+int pciinfoBarPhyAddr(char sysPathPciDev[], uint8_t barNo,
+                      uint32_t *barPhyAddr);
+
+
+
+/** @brief  set verbose level
+ *
+ *  enabling disabling of pciinfo additional debug information
+ *
+ *  @param[in]  uint8_t   PCI device vendor
+ *  @return     int       state
+ *  @retval     0         OK
+ *  @since      2018-05-25
+ *  @author     Andreas Kaeberlein
  */
 void pciinfoSetVerboseLevel(uint8_t level);
-
 
 //--------------------------------------------------------------
 #endif // __PCI_PCIINFO
